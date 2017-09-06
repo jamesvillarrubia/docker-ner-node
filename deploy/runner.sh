83b5c8626dea846b179812f43c6cbef5ad7e32e1
James Villarrubia <james.villarrubia@gmail.com>
2017-09-06 16:26:56 -0400

    no message
    Former-commit-id: 0d26d0b6802b35f8726046c6882bd28f44b094e3

--- deploy/runner.sh
+++ deploy/runner.sh
@@ -1,9 +1,9 @@
-
-
+AWS_ACCESS_KEY_ID=AKIAIRHUYTZB6A7N7SIA \
+AWS_SECRET_ACCESS_KEY=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 \
 REGION=us-east-1 \
 CLUSTER=cluster-ner-node \
 SERVICE=service-ner-node \
 CONTAINER=container-ner-node \
-IMAGE=my-user/my-repo \
+IMAGE=jamesmtc/ner-node \
 IMAGE_TAG=latest \
 ecs-deploy
