58fc7a67c15f2ecb9f26e473a0f0aee8eb0fceab
James Villarrubia <james.villarrubia@gmail.com>
2017-09-06 16:46:17 -0400

    yes
    Former-commit-id: 2e2917003d8f20a46a0d637d79549bbe14d15098

--- deploy/docker_push.sh
+++
@@ -1,8 +0,0 @@
-
-DOCKER_USER="jamesmtc"
-DOCKER_PASS="1Rekcod@kbw17"
-DOCKER_IMAGE_NAME="ner-node"
-docker build . -t "jamesmtc/$DOCKER_IMAGE_NAME"
-docker login --username=$DOCKER_USER --password=$DOCKER_PASS
-docker tag $DOCKER_IMAGE_NAME $DOCKER_USER/$DOCKER_IMAGE_NAME
-docker push $DOCKER_USER/$DOCKER_IMAGE_NAME
