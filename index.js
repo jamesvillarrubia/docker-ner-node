1185c76ce0215666f037fe59623907645bde8b94
James Villarrubia <james.villarrubia@gmail.com>
2017-09-06 13:16:07 -0400

    no message
    Former-commit-id: 78610d7c6971df3981444a67484843db4351f8a6

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json()); // for parsing application/json
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var NER = socketNER(1234, 'classifiers/english.muc.7class.distsim.crf.ser.gz', "./stanford-ner-2015-04-20/")
 NER.init()
 console.log('NER intialized')
@@ -28,10 +34,15 @@ app.get('/',function(req, res) {
 
 app.post('/', function(req, res) {
   //console.log('NER Request received')
-  var output = []
+  var output
   var startTime = process.uptime()
-  console.log(req)
-  res.send(NER.getEntities(req.body.data, ""))
+  try{
+    output = NER.getEntities(req.body.data, "")
+  }catch(err){
+    NER.init()
+    output = NER.getEntities(req.body.data, "")
+  }
+  res.send(output)
   //console.log(process.uptime() - startTime, "ms for 23 lines")
 })
 
